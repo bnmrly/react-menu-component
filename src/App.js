acc9730d536d95b6e7f4677e6908e6d0e0a86d16
Ben Marley <benjaminjohnmarley@gmail.com>
2019-12-09 10:27:19 +0000

    Update mobile styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Menu from './components/Menu/Menu';
+import './App.css';
 
 function App() {
+  // I need something like this that I can add in app but i need to pass it down as props then I need to add a page to each link with lorem ipsum and add routes but start with home page hiding and showing then move on
+  // const [isHomePageVisible, setIsHomePageVisible] = useState(true);
   return (
-    <div className="app__wrapper">
+    <div className="app__container">
       <header className="app__header">
         <Menu />
       </header>
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -45,36 +45,57 @@ function Menu() {
         <div className="menu__container">
           <div className="user-info__container">
             <div className="user-info">
-              <div className="avatar__container">
-                <img className="avatar" src={avatar} alt="avatar" />
-              </div>
+              <i class="fas fa-user-circle fa-3x"></i>
               <ul className="user-list">
-                <li className="user-list__item">Benjamin</li>
+                <li className="user-list__item text--large">Benjamin</li>
                 <li className="user-list__item">Available Balance</li>
-                <li className="user-list__item">£3,800:00</li>
+                <li className="user-list__item">£3,800.00</li>
               </ul>
             </div>
           </div>
 
           <nav className="nav">
             <ul className="nav__list">
-              <li className="nav-list__item">Profile</li>
-              <li className="nav-list__item">My Bookings</li>
-              <li className="nav-list__item">My Payments</li>
-              <li className="nav-list__item">Support</li>
-              <li className="nav-list__item">Contact Us</li>
-              <li className="nav-list__item">Log Out</li>
-              <li className="nav-list__item">About</li>
-              <li className="nav-list__item">FAQ</li>
+              <li className="nav-list__item">
+                <i class="fas fa-user"></i>Profile
+              </li>
+              <li className="nav-list__item">
+                <i class="fas fa-plane-departure"></i>My Bookings
+              </li>
+              <li className="nav-list__item">
+                <i class="fas fa-credit-card"></i>My Payments
+              </li>
+              <li className="nav-list__item">
+                <i class="fas fa-life-ring"></i>Support
+              </li>
+              <li className="nav-list__item">
+                <i class="fas fa-phone-alt"></i>Contact Us
+              </li>
+              <li className="nav-list__item">
+                <i class="fas fa-sign-out-alt"></i>Log Out
+              </li>
+              <li className="nav-list__item">
+                <i class="fas fa-question-circle"></i>About
+              </li>
+              <li className="nav-list__item">
+                <i class="fas fa-info"></i>FAQ
+              </li>
             </ul>
           </nav>
           <section className="contact-details">
             <ul className="contact-details__list">
               <li className="contact-details-list__item">We're here to help</li>
-              <li className="contact-details-list__item">
+              <li className="contact-details-list__item text--small">
                 +44 (0) 20 8050 3459
               </li>
-              <li className="contact-details-list__item">support@awaymo.com</li>
+              <li className="contact-details-list__item text--small">
+                <a
+                  href="mailto:support@awaymo.com?subject=Support Request"
+                  className="contact-details-list-item__link"
+                >
+                  support@awaymo.com
+                </a>
+              </li>
             </ul>
           </section>
         </div>
